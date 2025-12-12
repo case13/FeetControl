77f0664b7dfc9963fbc6b4797f073f39558f58df
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2025-12-11 22:08:21 -0300

    Criando Projetos

---
+++ Backend/FeetControl.Application/Configurations/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FeetControl.Infrastructure.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //services.AddDbContext<FeetControlDbContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Repositórios
+            // services.AddScoped<IVehicleRepository, VehicleRepository>();
+            // services.AddScoped<IDriverRepository, DriverRepository>();
+            // etc.
+
+            return services;
+        }
+    }
+}
