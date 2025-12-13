ac6bb9aa213473a8eee4b30bd7658b9ff0d1577a
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2025-12-13 18:30:08 -0300

    Create Helpers and EnumExtensions

---
+++ Shared/FeetControl.Shared/Helpers/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace FeetControl.Shared.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName<TEnum>(this TEnum value) where TEnum : System.Enum
+        {
+            var member = value.GetType()
+                .GetMember(value.ToString())
+                .FirstOrDefault();
+
+            if (member is null)
+                return value.ToString();
+
+            var display = member.GetCustomAttribute<DisplayAttribute>();
+            if (display is null)
+                return value.ToString();
+
+            return display.GetName() ?? value.ToString();
+        }
+    }
+}
