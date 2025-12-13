f1a4bfb46051f1770dd3e090a14fc227931271c6
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2025-12-13 18:29:44 -0300

    Create Enums

---
+++ Shared/FeetControl.Shared/Enums/DocumentTypeDriverEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeetControl.Shared.Enums
+{
+    public enum DocumentTypeDriverEnum
+    {
+        [Display(Name = "CNH")]
+        DriverLicense = 1,
+
+        [Display(Name = "Exame médico")]
+        MedicalExam = 2,
+
+        [Display(Name = "Exame psicológico")]
+        PsychologicalExam = 3,
+
+        [Display(Name = "Certificado de treinamento")]
+        TrainingCertificate = 4,
+
+        [Display(Name = "Outro")]
+        Other = 99
+    }
+}
---
+++ Shared/FeetControl.Shared/Enums/DocumentTypeVehicleEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeetControl.Shared.Enums
+{
+    public enum DocumentTypeVehicleEnum
+    {
+        [Display(Name = "Licenciamento (CRLV)")]
+        Licensing = 1,
+
+        [Display(Name = "IPVA")]
+        IPVA = 2,
+
+        [Display(Name = "Seguro")]
+        Insurance = 3,
+
+        [Display(Name = "Vistoria")]
+        Inspection = 4,
+
+        [Display(Name = "Outro")]
+        Other = 99
+    }
+}
